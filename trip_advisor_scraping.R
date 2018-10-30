9418d3f9116a3f3e5eb7f43a9f0881e7a2f0fb05
alexisidlettewilson <aiw@comcast.net>
2018-10-29 22:39:40 -0400

    moved to kids website

--- trip_advisor_scraping.R
+++ trip_advisor_scraping.R
@@ -8,20 +8,22 @@ library(lubridate)
 library(stringr)
 library(rvest)
 
-## studied TripAdvisor.com pages to determine results are grouped by 50
-## https://www.tripadvisor.com/VacationRentals-g34438-Reviews-Miami_Florida-Vacation_Rentals.html
-##https://www.tripadvisor.com/VacationRentals-g34438-Reviews-oa50-Miami_Florida-Vacation_Rentals.html
-## https://www.tripadvisor.com/VacationRentals-g34438-Reviews-oa100-Miami_Florida-Vacation_Rentals.html
-
-
-url1 <- ("https://www.tripadvisor.com/VacationRentals-g34438-Reviews")
-url2 <- ("-Miami_Florida-Vacation_Rentals.html")
-urlPages <- c("","oa50","oa100","oa150")
-
-## Find the maximum number of pages
-url="https://www.tripadvisor.com/VacationRentals-g34438-Reviews-Miami_Florida-Vacation_Rentals.html"
+url="http://fun4firstcoastkids.com/calendar/eventsbyday/2018/10/29/-"
 getHTML <- read_html(url)
-HTMLnodes <- html_node(getHTML,'.vr_listing')
-results <- html_text(HTMLnodes)
-results
-class(results)
+HTMLnodes <- html_nodes(getHTML,'.ev_link_row')
+titles <- as.data.frame(html_text(HTMLnodes))
+head(titles)
+
+HTMLnodesTime <- html_nodes(getHTML,'.f4us_time')
+eventTime <- as.data.frame(html_text(HTMLnodesTime))
+head(eventTime)
+
+HTMLnodesCost <- html_nodes(getHTML, '.f4us_cost')
+eventCost <- as.data.frame(html_text(HTMLnodesCost))
+head(eventCost)
+
+## unresolved
+events102918 <- cbind(titles, eventTime, eventCost)
+head(events102918)
+
+
